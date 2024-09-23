2bf6cf0543bc475adf41e4ce3c59b9243cf7f8bd
mekanio20 <mrxyok138@gmail.com>
2024-09-23 18:44:47 +0500

    product subcategories

--- frontend/src/views/admin/ProductsView.vue
+++ frontend/src/views/admin/ProductsView.vue
@@ -177,6 +177,14 @@
                         </select>
                     </div>
                     <div class="mb-5">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subcategory</label>
+                        <select v-model="subcategoryId"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option v-for="item in subcategories" :key="item.id" :value="item.id">{{ item.name_ru }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="mb-5">
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
                         <select v-model="brandId"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -239,6 +247,8 @@ export default {
             categoryId: null,
             brandId: null,
             categories: null,
+            subcategories: null,
+            subcategoryId: null,
             brands: null,
             currentPage: 1,
         }
@@ -270,6 +280,10 @@ export default {
             const data = await api.get('/brands')
             this.brands = data.data.detail.rows
         },
+        async allSubcategories() {
+            const data = await api.get('/subcategories')
+            this.subcategories = data.data.detail.rows
+        },
         // ##############
         async addProduct() {
             try {
@@ -289,6 +303,7 @@ export default {
                 formData.append('discount_type', this.discount_type);
                 formData.append('discount_price', this.discount_price);
                 formData.append('categoryId', this.categoryId);
+                formData.append('subcategoryId', this.subcategoryId);
                 formData.append('brandId', this.brandId);
                 if (this.selectedFiles) {
                     this.selectedFiles.forEach((file, index) => {
