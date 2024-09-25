d2562d4655cb0ae06dc016294975c2bb1a9c4c5d
mekanio20 <mrxyok138@gmail.com>
2024-09-25 18:13:20 +0500

    detail adaptive added

--- frontend/src/views/client/ProductDetailView.vue
+++ frontend/src/views/client/ProductDetailView.vue
@@ -10,27 +10,27 @@
                     class=" font-sf_pro font-normal md:text-xl sm:text-lg text-base text-m_gray-200">{{
                     product?.name_en }}</router-link>
             </div>
-            <div class="w-full flex items-start space-x-10 my-10">
+            <div class="w-full flex lg:flex-row flex-col lg:space-y-0 space-y-10 items-start lg:space-x-10 my-10">
                 <div class="flex-1 flex flex-col space-y-4">
                     <div class="w-full bg-m_gray-100 rounded-lg">
                         <img class="p-16" crossorigin="anonymous"
                             :src="`${$uploadUrl}/${product?.product_images[0].img}`">
                     </div>
-                    <div class="grid grid-cols-4 gap-x-4">
+                    <div class="grid grid-cols-4 gap-4">
                         <div class="bg-m_gray-100 rounded-lg" v-for="item in product?.product_images" :key="item.id">
                             <img class="p-4" crossorigin="anonymous" :src="`${$uploadUrl}/${item.img}`">
                         </div>
                     </div>
                 </div>
-                <div class="flex-1 flex flex-col space-y-6">
+                <div class="lg:flex-1 w-full flex flex-col space-y-6">
                     <div
-                        class="w-fit rounded-2xl px-6 py-2 font-sf_pro font-medium text-base text-m_orange-100 bg-m_orange-200">
+                        class="w-fit rounded-2xl px-6 py-2 font-sf_pro font-medium md:text-base sm:text-sm text-xs text-m_orange-100 bg-m_orange-200">
                         {{ product?.inStock === true ? 'In stock' : 'Out stock' }}</div>
-                    <h1 class="font-sf_pro font-bold text-4xl">{{ product?.name_en }}</h1>
+                    <h1 class="font-sf_pro font-bold md:text-4xl sm:text-3xl text-2xl">{{ product?.name_en }}</h1>
                     <div class="flex items-center space-x-6">
-                        <p class="font-sf_pro font-bold text-3xl text-m_red-200">{{ product?.final_price }} TMT</p>
+                        <p class="font-sf_pro font-bold md:text-3xl sm:text-2xl text-xl text-m_red-200">{{ product?.final_price }} TMT</p>
                         <p v-if="product?.discount_price > 0"
-                            class="font-sf_pro font-bold text-2xl text-m_gray-300 line-through">{{ product?.sale_price
+                            class="font-sf_pro font-bold md:text-2xl sm:text-xl text-lg text-m_gray-300 line-through">{{ product?.sale_price
                             }}
                             TMT</p>
                     </div>
@@ -39,24 +39,24 @@
                             <table class="w-full text-left font-sf_pro text-gray-700">
                                 <tbody>
                                     <tr>
-                                        <td class="font-bold py-2">Brand</td>
+                                        <td class="font-bold py-2 md:text-base sm:text-sm text-xs">Brand</td>
                                         <td class="py-2">{{ product?.brand.title }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="font-bold py-2">Model</td>
+                                        <td class="font-bold py-2 md:text-base sm:text-sm text-xs">Model</td>
                                         <td class="py-2">{{ product?.model }}
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td class="font-bold py-2">Year</td>
+                                        <td class="font-bold py-2 md:text-base sm:text-sm text-xs">Year</td>
                                         <td class="py-2">{{ product?.year }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="font-bold py-2">Made in</td>
+                                        <td class="font-bold py-2 md:text-base sm:text-sm text-xs">Made in</td>
                                         <td class="py-2">{{ product?.madeIn }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="font-bold py-2 pr-4">Category</td>
+                                        <td class="font-bold py-2 md:text-base sm:text-sm text-xs pr-4">Category</td>
                                         <td class="py-2">{{ product?.category?.name_en }}</td>
                                     </tr>
                                 </tbody>
@@ -65,7 +65,7 @@
                     </div>
                 </div>
             </div>
-            <div class="w-full my-20">
+            <div class="w-full sm:my-20 mb-10">
                 <h3 class="font-sf_pro font-bold md:text-3xl sm:text-2xl text-xl">Description</h3>
                 <p class="font-sf_pro font-normal md:text-lg sm:text-base text-sm text-m_gray-600 my-5">{{ product?.desc_en }}</p>
             </div>
@@ -148,7 +148,6 @@ export default {
     methods: {
         async getProduct() {
             const data = await api.get(`/product/${this.$route.params.id}`)
-            console.log(data);
             this.product = data.data.detail
         },
         async allProducts() {
